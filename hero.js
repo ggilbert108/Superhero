c233385821f15afcf62f916764efa97d52330f1c
Gaston Gilbert <ggilbert1@harding.edu>
2015-05-01 12:10:27 -0500

    Disabled button on click

--- hero.js
+++ hero.js
@@ -2,11 +2,14 @@ var imgNum = 0;
 
 $(document).ready(function(){
 	$("#next_button").click(function(){
+		$(this).prop("disabled", true);
 		imgNum = (imgNum + 1) % 5;
 		var id = "#day_" + (imgNum + 1) + "_img";
 
 		$("#day_img").fadeOut(function(){
 			$(this).attr("src", $(id).attr("src"));
-		}).fadeIn();
+		}).fadeIn(function(){
+			$("#next_button").prop("disabled", false);
+		});
 	});
 });
