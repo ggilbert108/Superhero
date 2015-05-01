f1a3505b699de45dd842ccd79d0173168206193a
Gaston Gilbert <ggilbert1@harding.edu>
2015-05-01 12:57:23 -0500

    Stuff better

--- hero.js
+++ hero.js
@@ -1,9 +1,10 @@
 var imgNum = 0;
-
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", ];
 $(document).ready(function(){
 	$("#next_button").click(function(){
 		$(this).prop("disabled", true);
 		imgNum = (imgNum + 1) % 5;
+		$("#day_tag").text(days[imgNum]);		
 		var id = "#day_" + (imgNum + 1) + "_img";
 
 		$("#day_img").fadeOut(function(){
